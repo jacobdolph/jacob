1f46abee25957d0d6c16e2bed3653742e6c98708
Jacob Dolph <jacob.dolph702@gmail.com>
2020-07-22 13:48:32 -0600

    adding new project to portfolio

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -9,7 +9,7 @@ function Card(props) {
 
             <div className="card" style={{ border: 'solid thin ', margin: '0 1rem 3rem 1rem' }}>
                 <div className="card-image">
-                    <img src={props.img} alt='' />
+                    <img src={props.img} alt='application image' style={{ height: "175px", overflow: "hidden" }} />
                 </div>
                 <div className="card-stacked" style={{ padding: '1rem 0 0 2rem' }}>
                     <span className="card-title activator grey-text text-darken-2" style={{ padding: '0 0 0 0' }} >{props.title}<i className="material-icons right">more_vert</i></span>
